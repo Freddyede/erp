9a226dd925e9b0f1f9e23495bdbf581007548de1
Freddyede <patouillardfranck3@gmail.com>
2023-12-12 16:49:26 +0100

    Update dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,16 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\{HttpFoundation\Response, Routing\Annotation\Route, Security\Http\Attribute\IsGranted};
 
+#[Route('/admin', name: 'admin.')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/', name: 'index')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'titlePages' => 'Dashboard'
         ]);
     }
 }
